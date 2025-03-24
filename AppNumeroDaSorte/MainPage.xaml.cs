2e9ec2ce2020d6cae42df0b4b84ebd18934bb72b
fillipe.felix <felipesoares_1993@hotmail.com>
2025-03-24 19:22:54 -0300

    Adicionar recurso de geração de números da sorte
    Implementou um recurso para gerar e exibir seis números da sorte exclusivos. Atualizou a IU para incluir um botão que aciona a geração e ajustou a visibilidade do layout. Esquema de cores modificado para melhorar a aparência do aplicativo.

--- AppNumeroDaSorte/MainPage.xaml.cs
+++ AppNumeroDaSorte/MainPage.xaml.cs
@@ -6,4 +6,30 @@ public partial class MainPage : ContentPage
     {
         InitializeComponent();
     }
+
+    private void OnGenerateLuckyNumbers(object sender, EventArgs e)
+    {
+        NameApp.IsVisible = false;
+        ContainerLuckNumbers.IsVisible = true;
+    
+        var luckyNumbers = GenerateUniqueRandomNumbers();
+        LuckNumber01.Text = luckyNumbers[0].ToString("D2");
+        LuckNumber02.Text = luckyNumbers[1].ToString("D2");
+        LuckNumber03.Text = luckyNumbers[2].ToString("D2");
+        LuckNumber04.Text = luckyNumbers[3].ToString("D2");
+        LuckNumber05.Text = luckyNumbers[4].ToString("D2");
+        LuckNumber06.Text = luckyNumbers[5].ToString("D2");
+    }
+    private List<int> GenerateUniqueRandomNumbers()
+    {
+        var random = new Random();
+        var numbers = new SortedSet<int>();
+
+        while (numbers.Count < 6)
+        {
+            numbers.Add(random.Next(1, 61));
+        }
+
+        return numbers.ToList();
+    }
 }
