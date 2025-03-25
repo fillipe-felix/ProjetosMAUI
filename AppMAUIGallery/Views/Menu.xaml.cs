363fb6a53741e1057b4bd390818c55429ac395ff
fillipe.felix <felipesoares_1993@hotmail.com>
2025-03-24 22:43:13 -0300

    Criação inicial do menu lateral

--- AppMAUIGallery/Views/Menu.xaml.cs
+++ AppMAUIGallery/Views/Menu.xaml.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+using AppMAUIGallery.Repositories;
+
 namespace AppMAUIGallery.Views;
 
 public partial class Menu : ContentPage
@@ -11,8 +13,31 @@ public partial class Menu : ContentPage
     public Menu()
     {
         InitializeComponent();
-        
-        
+
+        var categories = new CategoryRepository().GetCategories();
+
+        foreach (var category in categories)
+        {
+            var lblCategory = new Label();
+            lblCategory.Text = category.Name;
+            lblCategory.FontFamily = "OpenSansSemibold";
+
+            MenuContainer.Children.Add(lblCategory);
+            foreach (var component in category.Components)
+            {
+                var lblComponentTitle = new Label();
+                lblComponentTitle.Text = component.Title;
+                lblComponentTitle.FontFamily = "OpenSansSemibold";
+                lblComponentTitle.Margin = new Thickness(20, 10, 0, 0);
+                
+                var lblComponentDescription = new Label();
+                lblComponentDescription.Text = component.Description;
+                lblComponentDescription.Margin = new Thickness(20, 10, 0, 0);
+                
+                MenuContainer.Children.Add(lblComponentTitle);
+                MenuContainer.Children.Add(lblComponentDescription);
+            }
+        }
     }
 }
 
