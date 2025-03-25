8dcac6c229792ac9d81de08ae9070d739c372773
fillipe.felix <felipesoares_1993@hotmail.com>
2025-03-24 21:58:21 -0300

    Estrutura base basica do projeto

---
+++ AppMAUIGallery/Views/Menu.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppMAUIGallery.Views;
+
+public partial class Menu : ContentPage
+{
+    public Menu()
+    {
+        InitializeComponent();
+    }
+}
+
