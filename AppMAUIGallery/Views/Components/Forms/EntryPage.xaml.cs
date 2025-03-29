15a66e1127556ba839cb33082d1ca933f8142be7
fillipe.felix <felipesoares_1993@hotmail.com>
2025-03-29 20:00:55 -0300

    Criação do componente Entry e suas caracteristicas.

---
+++ AppMAUIGallery/Views/Components/Forms/EntryPage.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppMAUIGallery.Views.Components.Forms;
+
+public partial class EntryPage : ContentPage
+{
+    public EntryPage()
+    {
+        InitializeComponent();
+    }
+
+    private void InputView_OnTextChanged(object? sender, TextChangedEventArgs e)
+    {
+        LblText.Text = $"Antigo: {e.OldTextValue} - Novo: {e.NewTextValue}";
+    }
+
+    private void Entry_OnCompleted(object? sender, EventArgs e)
+    {
+        LblText.Text = $"Concluido!";
+    }
+}
+
